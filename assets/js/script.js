db2659920c2e627a6a4ae9a48696806050503b72
eddy goto <eduardogoto8@gmail.com>
2023-02-10 23:40:26 -0800

    updated html and got functionality of search to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,50 @@
 const apiKey = "a5830e97a03510fcdb8d20f7a0bbd55c";
 
-var cityEl = document.querySelector("#city");
-var dateEl = document.querySelector("#date");
-var weatherIconEl = document.querySelector("#weather-icon");
-var temperatureEl = document.querySelector("#temperature");
-var humidityEl = document.querySelector("#humidity");
-var windEl = document.querySelector("#wind");
-var uvIndexEl = document.querySelector("#uv-index");
+const cityInput = document.querySelector("#city-input");
+const searchButton = document.querySelector("#search-btn");
+const clearButton = document.querySelector("#clear-btn");
+const currentCity = document.querySelector("#current-city");
+const currentTemperature = document.querySelector("#temperature");
+const currentHumidity = document.querySelector("#humidity");
+const currentWindSpeed = document.querySelector("#wind-speed");
+const currentUVIndex = document.querySelector("#uv-index");
 
-var cityList = document.querySelector("#recent-searches");
-var cityInput = document.querySelector("#city-input");
+const formSubmitHandler = function (event) {
+  event.preventDefault();
 
-var listed = [];
+  let city = cityInput.value.trim();
 
-function loadCities() {
-    var storedCities = JSON.parse(localStorage.getItem("listed"));
-    if (storedCities) {
-        listed = storedCities
+  if (city) {
+    getWeather(city);
+
+    cityInput.value = " ";
+  } else {
+    alert("Please enter a city to search");
+  }
+};
+
+const getWeather = function (city) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+  fetch(url).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        let date = new Date(data.dt*1000);
+        date = date.toLocaleDateString();
+
+        currentCity.textContent = `${data.name} (${date})`;
+        currentTemperature.textContent = `${data.main.temp} F`;
+        currentHumidity.textContent = `${data.main.humidity} %`;
+        currentWindSpeed.textContent = `${data.wind.speed} MPH`;
+      });
+    } else {
+      alert("Error: " + response.statusText + " when making a request to Open Weather Map.");
     }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Open Weather Map");
+  });
 }
 
-function storeCities() {
-    localStorage.setItem("listed", JSON.stringify(listed));
-}
+searchButton.addEventListener("click", formSubmitHandler);
 
