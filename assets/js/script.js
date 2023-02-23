90b50c2c79e002bede2a26f362bcd0b73d1b8abe
eddy goto <eduardogoto8@gmail.com>
2023-02-22 21:20:51 -0800

    updated city list function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ const currentTemperature = document.querySelector("#temperature");
 const currentHumidity = document.querySelector("#humidity");
 const currentWindSpeed = document.querySelector("#wind-speed");
 const currentUVIndex = document.querySelector("#uv-index");
+const cityList = document.querySelector("#city-list");
+
+let listedCities = [];
 
 const formSubmitHandler = function (event) {
   event.preventDefault();
@@ -36,6 +39,9 @@ const getWeather = function (city) {
         currentTemperature.textContent = `${data.main.temp} F`;
         currentHumidity.textContent = `${data.main.humidity} %`;
         currentWindSpeed.textContent = `${data.wind.speed} MPH`;
+        currentUVIndex.textContent = `${data.uvi}`;
+
+        updateListedCities(data.name);
       });
     } else {
       alert("Error: " + response.statusText + " when making a request to Open Weather Map.");
@@ -46,5 +52,61 @@ const getWeather = function (city) {
   });
 }
 
-searchButton.addEventListener("click", formSubmitHandler);
+const updateListedCities = function(city) {
+  if (city.length) {
+    let cityCheck = false;
+    for (let i = 0; i < listedCities.length; i++) {
+      if (listedCities[i] === city) {
+        cityCheck = true;
+      }
+    }
+    if (!cityCheck) {
+      listedCities.push(city);
+      localStorage.setItem("listedCities", JSON.stringify(listedCities)); addButton(city);
+    }
+  } else {
+    listedCities.push(city);
+    localStorage.setItem("listedCities", JSON.stringify(city));
+    renderListedCities();
+  }
+}
 
+function init () {
+  const storedCities = JSON.parse(localStorage.getItem("listedCities"));
+  if (storedCities !== null) {
+    listedCities = storedCities;
+    renderListedCities();
+  }
+}
+
+function addButton(text) {
+  const newButton = document.createElement("btn");
+  const newList = document.createElement("li");
+
+  newButton.textContent = text;
+  newButton.addEventListener("click", newButtonClickHandler);
+  newList.appendChild(newButton);
+  cityList.appendChild(newList);
+}
+
+const newButtonClickHandler = function(event) {
+  event.preventDefault();
+  getWeather(event.srcElement.textContent);
+}
+
+function renderListedCities() {
+  for (let i = 0; i < listedCities.length; i++) {
+    addButton(listedCities[i]);
+  }
+}
+
+const clearHistoryButtonHandler = function(event) {
+  event.preventDefault();
+  localStorage.clear();
+  cityList.textContent = "";
+}
+
+searchButton.addEventListener("click", formSubmitHandler);
+cityList.addEventListener("click", newButtonClickHandler);
+clearButton.addEventListener("click", clearHistoryButtonHandler);
+init();
